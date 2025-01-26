5990bec301a833d88290b61361e21374097ad391
Siddhant Jain <siddhant.jain18@gmail.com>
2025-01-26 10:18:57 +0000

    another commit

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 from thyroid_disease_det.pipeline.training_pipeline import TrainPipeline
 
 
-pipline  = TrainPipeline()
-pipline.run_pipeline()
+pipeline  = TrainPipeline()
+pipeline.run_pipeline()
