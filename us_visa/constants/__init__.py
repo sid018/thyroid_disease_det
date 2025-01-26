052ddbcb217a813bddee78440dc3d9b298ee3d65
sid018 <siddhant.jain18@gmail.com>
2025-01-26 07:16:51 +0000

    Initial commit

---
+++ us_visa/constants/__init__.py
@@ -0,0 +1,38 @@
+import os
+from datetime import date
+
+DATABASE_NAME = "THYROID"
+
+# Suggested code may be subject to a license. Learn more: ~LicenseLog:1060015198.
+COLLECTION_NAME = "thyroid_data"
+
+MONGODB_URL_KEY = "MONGODB_URL"
+
+PIPELINE_NAME: str = "usvisa"
+ARTIFACT_DIR: str = "artifact"
+
+MODEL_FILE_NAME = "model.pkl"
+
+TARGET_COLUMN = "Class"
+
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+FILE_NAME: str = "hypothyroid.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
+
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "thyroid_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
