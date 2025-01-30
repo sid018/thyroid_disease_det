645743fac58040d3905dfa72c10b63d76cf9b44c
Siddhant Jain <siddhant.jain18@gmail.com>
2025-01-30 08:19:14 +0000

    anohte commit

--- thyroid_disease_det/pipeline/training_pipeline.py
+++ thyroid_disease_det/pipeline/training_pipeline.py
@@ -5,15 +5,16 @@ from thyroid_disease_det.logger import logging
 from thyroid_disease_det.components.data_ingestion import DataIngestion
 
 
-from thyroid_disease_det.entity.config_entity import (DataIngestionConfig)
+from thyroid_disease_det.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 
 
-from thyroid_disease_det.entity.artifact_entity import (DataIngestionArtifact)
+from thyroid_disease_det.entity.artifact_entity import (DataIngestionArtifact, DataValidationArtifact)
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+         self.data_validation_config = DataValidationConfig()
 
 
 
@@ -39,13 +40,42 @@ class TrainPipeline:
 
 
 
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise thyroid_disease_detException(e, sys) from e       
+
+
+
+
 
     def run_pipeline(self, ) -> None:
         """
         This method of TrainPipeline class is responsible for running complete pipeline
         """
         try:
-            data_ingestion_artifact = self.start_data_ingestion()    
+            data_ingestion_artifact = self.start_data_ingestion() 
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+   
 
 
 
