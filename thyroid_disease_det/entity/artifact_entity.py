715aa9100ec0e603ce1aba4464948a282bbdb7d8
Siddhant Jain <siddhant.jain18@gmail.com>
2025-02-20 08:54:48 +0000

    another commit

--- thyroid_disease_det/entity/artifact_entity.py
+++ thyroid_disease_det/entity/artifact_entity.py
@@ -24,3 +24,19 @@ class DataTransformationArtifact:
     drift_status:bool
     message: str
     drift_report_file_path: str    
+
+
+
+
+@dataclass
+class ClassificationMetricArtifact:
+    f1_score:float
+    precision_score:float
+    recall_score:float
+
+
+
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path:str 
+    metric_artifact:ClassificationMetricArtifact    
--- thyroid_disease_det/pipeline/training_pipeline.py
+++ thyroid_disease_det/pipeline/training_pipeline.py
@@ -5,12 +5,19 @@ from thyroid_disease_det.logger import logging
 from thyroid_disease_det.components.data_ingestion import DataIngestion
 from thyroid_disease_det.components.data_validation import DataValidation
 from thyroid_disease_det.components.data_transformation import DataTransformation
+from us_visa.components.model_trainer import ModelTrainer
 
 
-from thyroid_disease_det.entity.config_entity import (DataIngestionConfig, DataValidationConfig,DataTransformationConfig)
+from thyroid_disease_det.entity.config_entity import (DataIngestionConfig, 
+                                                      DataValidationConfig,
+                                                      DataTransformationConfig,
+                                                      ModelTrainerConfig)
 
 
-from thyroid_disease_det.entity.artifact_entity import (DataIngestionArtifact, DataValidationArtifact,DataTransformationArtifact)
+from thyroid_disease_det.entity.artifact_entity import (DataIngestionArtifact, 
+                                                        DataValidationArtifact,
+                                                        DataTransformationArtifact,
+                                                        ModelTrainerArtifact)
 
 
 class TrainPipeline:
@@ -18,6 +25,8 @@ class TrainPipeline:
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
+
 
 
 
@@ -80,9 +89,23 @@ class TrainPipeline:
             data_transformation_artifact = data_transformation.initiate_data_transformation()
             return data_transformation_artifact
         except Exception as e:
-            raise thyroid_disease_detException(e, sys)           
+            raise thyroid_disease_detException(e, sys)  
 
 
+
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model training
+        """
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                         model_trainer_config=self.model_trainer_config
+                                         )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise USvisaException(e, sys)
 
 
 
@@ -95,7 +118,7 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(
                 data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
-   
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
 
 
 
